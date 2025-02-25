ac3efad28565994f7470e07dc1689d50b0955853
ali-consciousroad <ali_dindar@live.be>
2025-02-25 19:39:55 +0000

    Fix the useState() hook using light instead of a boolean, use a function form to update to state to avoid bugs and work always with the last state value, fix bug by using theme state everywhere instead of mixing theme and darkMode as the state value

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -4,22 +4,24 @@ import { createContext, useContext, useState } from "react";
 // Create a context
 const ThemeContext = createContext(undefined);
 
-// Implement the ThemeProvider component
+// Implement the ThemeProvider component with a children prop
 export const ThemeProvider = ({ children }) => {
     // Step 2. 
-    // Add the useState hook
-    const [darkMode, setDarkMode] = useState(false);
+    // Add the useState hook and add a default state
+    const [theme, setTheme] = useState("light");
 
     // Create the toogleTheme function
     const toggleTheme = () => {
-        setDarkMode(!darkMode);
+        /* Use of function form of the state updater instead of the state "theme" directly 
+        to ensure the state is based on the most recent state value and avoid bugs */
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
     };
 
     // Return a ThemeContext.Provider component 
     return (
         <ThemeContext.Provider
             value={{
-                darkMode,
+                theme, 
                 toggleTheme
             }}>
             {children}
